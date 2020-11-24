eb8830b0028e1f44e3a9c25813cef81c555ffa10
mikhael28 <mikhael@hey.com>
2020-11-23 18:19:03 -0800

    Sanity resources being fetched on project page

--- src/templates/project/project.tsx
+++ src/templates/project/project.tsx
@@ -1,7 +1,7 @@
 import BlockContent from "@sanity/block-content-to-react";
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql, Link } from "gatsby";
 import Img from "gatsby-image";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import IconGithub from "../../components/icons/icon-github";
 import IconLink from "../../components/icons/icon-link";
 import Layout from "../../components/layout/layout";
@@ -9,11 +9,28 @@ import Tag from "../../components/projects-section/tags/tag/tag";
 import SEO from "../../components/seo";
 import { IProject } from "../../contracts/projects/project/iproject";
 import { ITag } from "../../contracts/tags/tag/itag";
+import sanity from "../../sanity";
 import "./project.scss";
 
 const Project: FunctionComponent<{ data: { sanityProject: IProject } }> = ({
   data,
 }) => {
+  const [resources, setResources] = useState([]);
+  const [path, setPath] = useState("");
+
+  useEffect(() => {
+    fetchSanityResources();
+  }, []);
+
+  async function fetchSanityResources() {
+    let tempPath = window.location.pathname.split("/");
+    const schema = tempPath[2];
+    setPath(tempPath[2]);
+    const query = `*[_type == '${schema}Schema']`;
+    const links = await sanity.fetch(query);
+    setResources(links);
+  }
+  console.log("Sanity assets: ", resources);
   return (
     <Layout>
       <SEO
@@ -21,10 +38,10 @@ const Project: FunctionComponent<{ data: { sanityProject: IProject } }> = ({
         description={data.sanityProject.description}
       />
       <div className="project-container">
-        <Img
+        {/* <Img
           className="project-image"
           fluid={data.sanityProject.mainImage.asset.fluid}
-        />
+        /> */}
         <div className="project-content-container">
           <div className="project-heading-container">
             <h1 className="project-title">{data.sanityProject.title}</h1>
@@ -64,6 +81,20 @@ const Project: FunctionComponent<{ data: { sanityProject: IProject } }> = ({
             </div>
           </div>
         </div>
+        {resources.map((resource, index) => {
+          return (
+            <div>
+              <h1>{resource.title}</h1>
+              <BlockContent blocks={resource.overview} />
+              <Link
+                className="project-card"
+                to={`/developer/${path}/${resource.slug.current}`}
+              >
+                Click here to view resource.
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </Layout>
   );
